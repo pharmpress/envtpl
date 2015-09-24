7093c7c33bef1e558e158434dfd9d486eff9744e
ramiroCalle <ramiro.calle@rpharms.com>
2015-09-24 17:12:32 +0100

    add deploy on git script

--- build.sh
+++ build.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-set -x
+echo "building statically-linked envtpl..."
+docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:latest sh -c 'CGO_ENABLED=0 go build -v -o bin/envtpl-linux64 ; CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -v -o bin/envtpl-windows64.exe'
 
-rm -rf bin/
-docker build --rm -t envtpl .
-docker run --rm -v $PWD:/go/src/app envtpl
+RELEASE_TAG=v0.2.0
+
+case $1 in
+"deploy")
+
+go get github.com/aktau/github-release
+
+git tag $RELEASE_TAG && git push --tags
+
+github-release release --user pharmpress --repo envtpl --tag $RELEASE_TAG --name "envtpl$RELEASE_TAG" --description "my first release!" --pre-release
+
+github-release upload --user pharmpress --repo envtpl --tag $RELEASE_TAG --name "envtpl-linux64" --file bin/envtpl-linux64
+
+github-release upload --user pharmpress --repo envtpl --tag $RELEASE_TAG --name "envtpl-windows64.exe" --file bin/envtpl-windows64.exe
+
+;;
+esac
+
